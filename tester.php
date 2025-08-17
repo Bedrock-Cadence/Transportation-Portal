fb817ea34d9dcf2afd20a351285d7de32350fe8c
Alex <43963205+AHanadsaker@users.noreply.github.com>
2025-08-17 08:41:04 -0500

    new file:   tester.php

---
+++ tester.php
@@ -0,0 +1,11 @@
+<?php
+// --- TEMPORARY DEBUGGING ---
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+echo "Attempting to include init.php...<br>";
+
+require_once __DIR__ . '/../../app/init.php';
+
+echo "init.php was included successfully!";
