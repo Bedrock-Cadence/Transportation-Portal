41c30284c668de0e6a4f3aebca0d9843d94cc95a
Alex <43963205+AHanadsaker@users.noreply.github.com>
2025-08-17 08:45:17 -0500

    modified:   tester.php

--- tester.php
+++ tester.php
@@ -1,10 +1,11 @@
 <?php
+// FILE: public_html/portal/login.php (TESTING VERSION)
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-echo "Attempting to include the test file...<br>";
+echo "Attempting to include init.php...<br>";
 
-require_once __DIR__ . '/../../app/test_include.php';
+require_once __DIR__ . '/../../app/init.php';
 
-echo "<br>This message will only appear if the include worked.";
+echo "<br>init.php was included successfully!";
