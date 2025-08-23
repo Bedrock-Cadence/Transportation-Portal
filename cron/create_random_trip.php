2d7b8abcac38ef01fb4d552acfe6bb7ddbad4c24
Alex <43963205+AHanadsaker@users.noreply.github.com>
2025-08-23 08:28:42 -0500

    modified:   cron/create_random_trip.php

--- cron/create_random_trip.php
+++ cron/create_random_trip.php
@@ -14,7 +14,7 @@ require_once __DIR__ . '/../../../app/init.php';
 // This number controls the probability.
 // On average, a trip will be created 1 out of every CHANCE_FACTOR runs.
 // If the cron runs every minute, a value of 10 means about 1 trip every 10 minutes.
-const CHANCE_FACTOR = 5;
+const CHANCE_FACTOR = 7;
 // --- END CONFIGURATION ---
 
 // Decide if we should create a trip this time
